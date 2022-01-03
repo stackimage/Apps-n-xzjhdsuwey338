ae354ef48c10b9c4bdd4a532886a3cce792c070d
joxiplayer <joxiplayer.id@gmail.com>
2022-01-03 20:24:31 +0700

    first cimmit

---
+++ File-Download/Design-Aplikasi-Music/script.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper( '.swiper-container.two', {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		effect: 'coverflow',
+		loop: true,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflow: {
+			rotate: 0,
+			stretch: 100,
+			depth: 150,
+			modifier: 1.5,
+			slideShadows : false,
+		}
+} );
+
+var timeleft = 20;
+const downloadButton = document.querySelector(".down-btn");
+const countdown = document.querySelector(".countdown")
+const waitText = document.querySelector(".waiting-text")
+const downloadText = document.querySelector(".download-text")
+const downloadLink = document.querySelector(".download-link")
+
+downloadButton.addEventListener("click", () => {
+    downloadButton.style.display = "none";
+    countdown.innerHTML = "Download will begin automatically in <span>" + timeleft + " </span> seconds."; // For Fast Start
+
+    var downloadtimer = setInterval(function timecount() {
+        timeleft -= 1;
+        countdown.innerHTML = "Download will begin automatically in <span>" + timeleft + " </span> seconds.";
+        if (timeleft <= 0) {
+            clearInterval(downloadtimer);
+            waitText.style.display = "block";
+            let download_href = "Design Musik Apk - Stack Image.rar";
+            window.location.href = download_href;
+            downloadLink.href = download_href;
+
+            setTimeout(() => {
+                waitText.style.display = "none";
+                downloadText.style.display = "block";
+            }, 4000);
+        }
+    }, 1000);
+});
