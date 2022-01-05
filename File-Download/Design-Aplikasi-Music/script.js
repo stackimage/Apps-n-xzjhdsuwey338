5eb6c8fb5e237e0baa3e9285ed48624a9b921046
joxiplayer <joxiplayer.id@gmail.com>
2022-01-05 15:18:58 +0700

    first commit

--- File-Download/Design-Aplikasi-Music/script.js
+++ File-Download/Design-Aplikasi-Music/script.js
@@ -1,3 +1,22 @@
+const shareData = {
+    title: 'Stack Image',
+    text: 'Download Design Musik Aplikasi | Stack Image Link :',
+    url: 'https://stackimage.github.io'
+  }
+
+  const btn = document.querySelector('button');
+  const resultPara = document.querySelector('.result');
+
+  // Share must be triggered by "user activation"
+  btn.addEventListener('click', async () => {
+    try {
+      await navigator.share(shareData)
+      resultPara.textContent = 'MDN shared successfully'
+    } catch(err) {
+      resultPara.textContent = 'Error: ' + err
+    }
+  });
+
 var swiper = new Swiper( '.swiper-container.two', {
 		pagination: '.swiper-pagination',
 		paginationClickable: true,
